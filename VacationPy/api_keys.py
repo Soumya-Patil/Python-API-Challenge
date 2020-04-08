94a7e3de420646fd522a0eb194cd717cea845a65
Soumya Patil <getme1104@gmail.com>
2020-04-08 00:36:51 -0700

    Added Folder Structure

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "00fd69a2b58ea2c5ac43d328b103dd26"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyC-xtjp0zAIB7EzZCM32Dj8RL4MK3M90b4"
